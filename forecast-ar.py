65630d4e8a022ff00e3425d599f351cc27717fa2
lizapk <126601571+lizapk@users.noreply.github.com>
2023-10-30 21:16:01 +0700

    Add files via upload

---
+++ forecast-ar.py
@@ -0,0 +1,29 @@
+import pickle
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+model = pickle.load(open('forecast-ar.sav','rb'))
+
+df = pd.read_csv("AirPassengers.csv")
+df['Month'] = pd.to_datetime(df['Month'], format='%Y-%m')
+df.set_index(['Month'], inplace=True)
+
+st.title('Forecasting Penumpang Pesawat')
+month = st.slider("Tentukan bulan",1,12, step=1)
+
+pred = model.forecast(month)
+pred = pd.DataFrame(pred, columns=['#Passengers'])
+
+if st.button("Predict"):
+
+    col1, col2 = st.columns([2,3])
+    with col1:
+        st.dataframe(pred)
+    with col2:
+        fig, ax = plt.subplots()
+        df['#Passengers'].plot(style='--', color='gray', legend=True, label='known')
+        pred['#Passengers'].plot(color='b', legend=True, label='Prediction')
+        st.pyplot(fig)
+
+        
