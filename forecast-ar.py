3a7e1f0b9a0b3cf3eca3f8e332de7ee7ee032db6
lizapk <126601571+lizapk@users.noreply.github.com>
2023-10-31 11:02:32 +0700

    Update forecast-ar.py

--- forecast-ar.py
+++ forecast-ar.py
@@ -2,9 +2,12 @@ import pickle
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 
+# Load the model
 model = pickle.load(open('forecast-ar.sav', 'rb'))
 
+# Load the historical data
 df = pd.read_csv("AirPassengers.csv")
 df['Month'] = pd.to_datetime(df['Month'], format='%Y-%m')
 df.set_index(['Month'], inplace=True)
@@ -15,8 +18,12 @@ month = st.slider("Tentukan bulan", 1, 30, step=1)
 pred = model.forecast(month)
 pred = pd.DataFrame(pred, columns=['#Passengers'])
 
-if st.button("Predict"):
+# Generate date range for the predictions
+last_date = df.index[-1]
+date_range = [last_date + timedelta(days=i*30) for i in range(1, month+1)]
+pred['Month'] = date_range
 
+if st.button("Predict"):
     col1, col2 = st.columns([2, 3])
     with col1:
         st.dataframe(pred)
