5bd430b6634fb83b20a9c460716caa8a8a0992cd
lizapk <126601571+lizapk@users.noreply.github.com>
2023-10-31 11:11:44 +0700

    Update forecast-ar.py

--- forecast-ar.py
+++ forecast-ar.py
@@ -1,65 +1,53 @@
-# Import library yang diperlukan
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pickle
 import matplotlib.pyplot as plt
 from statsmodels.tsa.arima_model import ARIMA
 from statsmodels.tsa.seasonal import seasonal_decompose
 
-# Fungsi untuk memuat data dan model ARIMA
-def load_data_and_model(data_file, model_file):
-    # Muat data time series
-    data = pd.read_csv("AirPassengers.csv")
-    
-    # Muat model ARIMA
-    model = ARIMA
-    model = model.load("forecast-ar.sav")
-    
-    return data, model
+# Muat model ARIMA dari file .sav
+model_file = 'model_forecast-ar.sav'
+model = pickle.load(open(model_file, 'rb'))
 
-# Fungsi untuk menampilkan grafik
-def plot_data_and_forecast(data, model, forecast_steps):
+# Muat dataset AirPassengers.csv
+data_file = 'AirPassengers.csv'
+data = pd.read_csv(data_file)
+data['Month'] = pd.to_datetime(data['Month'])
+data.set_index('Month', inplace=True)
+
+# Judul aplikasi
+st.title('ARIMA Forecasting App')
+
+# Slider untuk menentukan jumlah bulan yang akan diprediksi
+forecast_steps = st.slider('Jumlah Bulan Prediksi', 1, 24, 12)
+
+# Tombol "Prediksi"
+if st.button('Prediksi'):
+    # Prediksi dengan model ARIMA
+    forecast, stderr, conf_int = model.forecast(steps=forecast_steps)
+
     # Dekomposisi musiman
     result = seasonal_decompose(data, model='additive')
-    
-    # Plot data asli
+
+    # Tampilkan data asli
     st.subheader('Data Asli')
     st.line_chart(data)
-    
-    # Plot hasil dekomposisi
+
+    # Tampilkan hasil dekomposisi
     st.subheader('Hasil Dekomposisi')
     st.line_chart(result.trend)
     st.line_chart(result.seasonal)
     st.line_chart(result.resid)
-    
-    # Prediksi
-    forecast, stderr, conf_int = model.forecast(steps=forecast_steps)
-    
-    # Plot hasil prediksi
+
+    # Tampilkan hasil prediksi
     st.subheader('Hasil Prediksi')
     st.line_chart(forecast)
-    
+
     # Tampilkan tabel hasil prediksi
     st.subheader('Tabel Hasil Prediksi')
     forecast_df = pd.DataFrame({
-        'Waktu': pd.date_range(start=data.index[-1], periods=forecast_steps + 1, closed='right'),
+        'Tanggal': pd.date_range(start=data.index[-1], periods=forecast_steps + 1, closed='right'),
         'Prediksi': forecast
     })
     st.dataframe(forecast_df)
-
-# Main Streamlit app
-def main():
-    st.title('ARIMA Forecasting App')
-    
-    data_file = st.file_uploader('Upload file data time series (CSV)', type=['csv'])
-    model_file = st.file_uploader('Upload file model ARIMA', type=['pkl'])
-    
-    if data_file is not None and model_file is not None:
-        data, model = load_data_and_model(data_file, model_file)
-        
-        forecast_steps = st.slider('Jumlah Langkah Prediksi', 1, 365, 30)
-        
-        plot_data_and_forecast(data, model, forecast_steps)
-
-if __name__ == '__main__':
-    main()
