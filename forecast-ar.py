1d78c84d18b523bfa988108b3f8fc58a02872cb4
lizapk <126601571+lizapk@users.noreply.github.com>
2023-10-31 07:42:15 +0700

    Update forecast-ar.py

--- forecast-ar.py
+++ forecast-ar.py
@@ -8,7 +8,6 @@ model = pickle.load(open('forecast-ar.sav','rb'))
 df = pd.read_csv("AirPassengers.csv")
 df['Month'] = pd.to_datetime(df['Month'], format='%Y-%m')
 df.set_index(['Month'], inplace=True)
-pred['#Passengers'] = pred['#Passengers'].astype(float)
 
 st.title('Forecasting Penumpang Pesawat')
 month = st.slider("Tentukan bulan",1,12, step=1)
@@ -18,7 +17,15 @@ pred = pd.DataFrame(pred, columns=['#Passengers'])
 
 if st.button("Predict"):
 
-    col1, col2 = st.columns([2,3])
+    col1, col2 = st.columns([2, 3])
+
+    # Convert the 'Month' column to a datetime format in the pred DataFrame
+    pred['Month'] = pd.date_range(start=df.index[-1], periods=month+1, freq='M')
+    pred.set_index(['Month'], inplace=True)
+
+    # Ensure 'Passengers' column is numeric
+    pred['#Passengers'] = pred['#Passengers'].astype(float)
+
     with col1:
         st.dataframe(pred)
     with col2:
@@ -26,5 +33,3 @@ if st.button("Predict"):
         df['#Passengers'].plot(style='--', color='gray', legend=True, label='known')
         pred['#Passengers'].plot(color='b', legend=True, label='Prediction')
         st.pyplot(fig)
-
-        
